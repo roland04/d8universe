7620d8fe868340ffecf4044aac3bcf2ef5e5f357
roland <mikelo.fmc@gmail.com>
2015-10-27 23:46:08 +0100

    app universes, inicio modelos y admin

--- d8universe/settings.py
+++ d8universe/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'universes',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -85,7 +86,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
