80f68204ff04f5585388c5b2bcc1788ae9830208
Mikel <mikel.martin@edooca.com>
2015-11-02 16:21:58 +0100

    Cambios en admin

--- universes/admin.py
+++ universes/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from .models import Universe, Atribute, Ability
 
-admin.site.register(Universe)
+class UniverseAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None,          {'fields': ['name']}),
+            ('Atributes',   {'fields': ['atributes'], 'classes': ['collapse']}),
+            ('Abilities',   {'fields': ['abilities'], 'classes': ['collapse']}),
+        ]
+    filter_horizontal = ('atributes','abilities')
+
+admin.site.register(Universe, UniverseAdmin)
 admin.site.register(Atribute)
 admin.site.register(Ability)
