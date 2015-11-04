0491ef782c9c736791d514c949ef1437b2c0d94d
roland <mikelo.fmc@gmail.com>
2015-11-04 23:08:34 +0100

    model

--- universes/admin.py
+++ universes/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Universe, Atribute, Ability
+from .models import Universe, Atribute, Ability, Race, StartAtribute
 
 admin.site.register(Universe)
 admin.site.register(Atribute)
 admin.site.register(Ability)
+admin.site.register(Race)
+admin.site.register(StartAtribute)
--- universes/models.py
+++ universes/models.py
@@ -15,9 +15,26 @@ class Ability(models.Model):
         return '%s (%s)' % (self.name, self.atribute.short_name)
 
 class Universe(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=120)
     atributes = models.ManyToManyField(Atribute)
     abilities = models.ManyToManyField(Ability)
+    HP_tag = models.CharField(max_length=20, default="Hit Points")
+    EP_tag = models.CharField(max_length=20, default="Energy Points")
 
     def __str__(self):
         return self.name
+
+class Race(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=120)
+    atributes = models.ManyToManyField(Atribute, through='StartAtribute')
+
+    def __str__(self):
+        return self.name
+
+class StartAtribute(models.Model):
+    race = models.ForeignKey(Race)
+    atribute = models.ForeignKey(Atribute)
+    universe = models.ForeignKey(Universe)
+    value = models.DecimalField(max_digits=3, decimal_places=0)
