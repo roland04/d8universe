27a8b584cbfc5775711cfdac75aee8b4132fbb98
Mikel <mikel.martin@edooca.com>
2015-11-20 14:00:38 +0100

    nuevo sistema de login, index básico

--- login/forms.py
+++ login/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(
         max_length=20, label='',
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'username',})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'username', 'required': True, 'autofocus': True,})
     )
     password = forms.CharField(
         max_length=20, label='',
--- login/views.py
+++ login/views.py
@@ -2,29 +2,27 @@ from django.shortcuts import render, render_to_response
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
-from login.forms import LoginForm
 
 def user_login(request):
     context = RequestContext(request)
 
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
-            if user:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponseRedirect('/')
-                else:
-                    return HttpResponse("Your d8universe account is disabled.")
-            else:
-                print ("Invalid login details")
-                return HttpResponse("Invalid login details supplied.")
-    else:
-        form = LoginForm()
+        username = request.POST['username']
+        password = request.POST['password']
 
-    return render_to_response('login/index.html', {'form':form}, context)
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                return HttpResponse("Your d8universe account is disabled.")
+        else:
+            print ("Invalid login details")
+            return HttpResponse("Invalid login details supplied.")
+
+    return render_to_response('website/index.html', {}, context)
 
 def user_logout(request):
     context = RequestContext(request)
